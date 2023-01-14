b2cdebfc99b4908e783e3929502e2010105f7dc6
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-01-14 17:09:41 -0300

    Corrigindo erros POST /participants

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.get("/participants", async (req, res) => {
   try {
     const participants = await db.collection("participants").find().toArray();
 
-    res.status(200).send(participants);
+    return res.status(200).send(participants);
   } catch (error) {
     res.send("Deu zica no servidor");
   }
@@ -36,18 +36,17 @@ app.post("/participants", async (req, res) => {
 
   const nameInUse = await db.collection("participants").findOne({ name });
 
-    if (nameInUse) return res.sendStatus(409);
+  if (nameInUse) return res.sendStatus(409);
 
   const participantsScheme = joi.object({
     name: joi.string().min(1).required(),
   });
 
-  const validation = participantsScheme.validate(name, { abortEarly: false });
+  const validation = participantsScheme.validate({name}, { abortEarly: false });
 
   if (validation.error) return res.status(422).send(validation.error.details);
 
   try {
-
     await db
       .collection("participants")
       .insertOne({ name, lastStatus: Date.now() });
@@ -73,11 +72,9 @@ app.post("/messages", async (req, res) => {
 
   if (!user) return res.status(422).send("You must type an User");
 
-  const nameInUse = await db
-      .collection("participants")
-      .findOne({ name: user });
+  const nameInUse = await db.collection("participants").findOne({ name: user });
 
-    if (!nameInUse) return res.sendStatus(422);
+  if (!nameInUse) return res.sendStatus(422);
 
   const messageScheme = joi.object({
     to: joi.string().min(1).required(),
@@ -94,7 +91,6 @@ app.post("/messages", async (req, res) => {
   if (validation.error) return res.status(422).send(validation.error.details);
 
   try {
-    
     await db.collection("messages").insertOne({
       from: user,
       to,
@@ -109,5 +105,29 @@ app.post("/messages", async (req, res) => {
     res.status(500).send("Message not sent");
   }
 });
+
+// app.get("/messages",async(req,res)=>{
+//     const {limit} = req.query
+//     const {user} = req.headers
+
+// })
+
+// app.post("/status", async (req, res) => {
+//   const { user } = req.headers;
+
+//   const nameInUse = await db.collection("participants").findOne({ name: user });
+
+//   if (!nameInUse) return res.sendStatus(404);
+
+//   try {
+//     await db
+//       .collection("participants")
+//       .updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+//     return res.status(200).send()  
+//   } catch (error) {
+//     console.log(error.message);
+//     res.status(500).send("LastStatus not updated");
+//   }
+// });
 
 app.listen(5000, () => console.log("API funfou suave"));
