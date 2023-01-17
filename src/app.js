bed2a3d5b63940c9ba3fbbd9c41ace6f451f5cbe
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-01-16 22:49:10 -0300

    Corrigindo bugs

--- src/app.js
+++ src/app.js
@@ -25,6 +25,11 @@ app.get("/participants", async (req, res) => {
   try {
     const participants = await db.collection("participants").find().toArray();
 
+    if (!participants)
+      return res
+        .status(404)
+        .send("Nao foram encontrados participantes cadastrados");
+
     return res.status(200).send(participants);
   } catch (error) {
     res.send("Deu zica no servidor");
@@ -33,10 +38,6 @@ app.get("/participants", async (req, res) => {
 
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
-
-  const nameInUse = await db.collection("participants").findOne({ name });
-
-  if (nameInUse) return res.sendStatus(409);
 
   const participantsScheme = joi.object({
     name: joi.string().min(1).required(),
@@ -50,6 +51,10 @@ app.post("/participants", async (req, res) => {
   if (validation.error) return res.status(422).send(validation.error.details);
 
   try {
+    const nameInUse = await db.collection("participants").findOne({ name });
+
+    if (nameInUse) return res.sendStatus(409);
+
     await db
       .collection("participants")
       .insertOne({ name, lastStatus: Date.now() });
@@ -62,7 +67,7 @@ app.post("/participants", async (req, res) => {
       time: dayjs().format("HH:mm:ss"),
     });
 
-    res.sendStatus(201);
+    res.status(201).send("OK");
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Deu algo errado no servidor");
@@ -78,31 +83,33 @@ app.post("/messages", async (req, res) => {
 
   if (!user) return res.status(422).send("You must type an User");
 
-  const nameInUse = await db.collection("participants").findOne({ name: user });
-
-  if (!nameInUse) return res.sendStatus(422);
-
   const messageScheme = joi.object({
+    from: joi.string().min(1).required(),
     to: joi.string().min(1).required(),
     text: joi.string().min(1).required(),
     type: joi.string().valid("message", "private_message").required(),
   });
 
-  const validation = messageScheme.validate(
-    { to, text, type },
+  const { error, value: newMessage } = messageScheme.validate(
+    { to, text, type, from: user },
     { abortEarly: false }
   );
 
-  if (validation.error) return res.status(422).send(validation.error.details);
+  if (error) {
+    const err = error.details.map((e) => e.message);
+    return res.status(422).send(err);
+  }
 
   try {
-    await db.collection("messages").insertOne({
-      from: user,
-      to,
-      text,
-      type,
-      time: dayjs().format("HH:mm:ss"),
-    });
+    const nameInUse = await db
+      .collection("participants")
+      .findOne({ name: user });
+
+    if (!nameInUse) return res.status(422).send("User not registered");
+
+    await db
+      .collection("messages")
+      .insertOne({ ...newMessage, time: dayjs().format("HH:mm:ss") });
 
     res.status(201).send("Sucess: Message sent!");
   } catch (error) {
@@ -112,40 +119,32 @@ app.post("/messages", async (req, res) => {
 });
 
 app.get("/messages", async (req, res) => {
-  const { limit } = req.query 
+  const { limit } = req.query;
   const { user } = req.headers;
+  
+  if ( isNaN(limit) && limit || parseInt(limit) || limit == 0) return res.sendStatus(422);
 
-  // if (!user) return res.status(422).send("User is required");
+  if (!user) return res.status(422).send("User is required");
 
-  // const nameInUse = await db.collection("participants").findOne({ name: user });
+  try {
+
+    // const nameInUse = await db.collection("participants").findOne({ name: user });
 
   // if (!nameInUse) return res.status(422).send("User not found");
 
-  // const limitScheme = joi.object({
-  //   limit: joi.number().positive(),
-  // });
-
-  // const validadeLimit = limitScheme.validate({ limit });
-
-  // if (validadeLimit.error)
-  //   return res.status(422).send(validadeLimit.error.details);
-
-  try {
     const mensagens = await db
       .collection("messages")
       .find({
         $or: [
-          { to: user, type: "private_message" },
-          { from: user, type: "private_message" },
+          { to: { $in: [user, "Todos"] } },
+          { from: user },
           { type: "message" },
           { type: "status" },
         ],
       })
       .toArray();
 
-    if (limit < 0 || limit === 0 || isNaN(limit)) {
-      return res.sendStatus(422);
-    } else if (limit > 0) {
+    if (limit) {
       return res.send(mensagens.slice(-limit).reverse());
     } else {
       res.send(mensagens.reverse());
@@ -156,45 +155,59 @@ app.get("/messages", async (req, res) => {
   }
 });
 
-app.post("/status", async (req, res) => {
+app.put("/status", async (req, res) => {
   const { user } = req.headers;
 
   if (!user) return res.status(422).send("User is required");
 
-  const nameInUse = await db.collection("participants").findOne({ name: user });
-
-  if (!nameInUse) return res.sendStatus(404);
-
   try {
+    
+    const nameInUse = await db.collection("participants").findOne({ name: user });
+  
+    if (!nameInUse) return res.sendStatus(404);
+
     await db
       .collection("participants")
-      .updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+      .updateOne(
+        { name: user },
+        { $set: { lastStatus: Date.now() } }
+      );
+
     return res.status(200).send("OK");
+
   } catch (error) {
     console.log(error.message);
     res.status(500).send("LastStatus not updated");
   }
+
 });
+
+//Remoçao Automatica dos Inativos
 setInterval(async () => {
   let hora = dayjs().format("HH:mm:ss");
+  const inactiveTime = Date.now() - 10000;
+  
   try {
-    const inactiveTime = Date.now() - 10000;
-    const filterRule = { lastStatus: { $lt: inactiveTime } };
-    const InactiveUsers = await db
+
+    const inactiveUsers = await db
       .collection("participants")
-      .find(filterRule)
+      .find({lastStatus: {$lte: inactiveTime }})
       .toArray();
-    InactiveUsers.map(async (user) => {
-      await db.collection("participants").deleteOne({ _id: ObjectId(user.id) });
-      const leaveRoomMessageUpdate = {
-        from: user.name,
-        to: "Todos",
-        text: "sai da sala...",
-        type: "status",
-        time: hora
-      };
-      await db.collection("messages").insertOne(leaveRoomMessageUpdate);
-    });
+
+    if(inactiveUsers.length>0) {
+      const inactiveMessage = inactiveUsers.map((user)=>{
+        return{
+          from:user.name,
+          to:"Todos",
+          text:"sai da sala...",
+          type:"status",
+          time: dayjs().format("HH:mm:ss")
+        }
+      })
+      await db.collection("messages").insertMany(inactiveMessage)
+      await db.collection("participants").deleteMany({lastStatus: {$lte: inactiveTime }})
+    }
+   
   } catch (error) {
     console.log(error.message);
     res.status(500).send("LeaveRoom Message not updated");
